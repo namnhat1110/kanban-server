754ad8e00f74517322544c611055a93bedd0fbbd
NhatNam <namnhat1110@gmail.com>
2024-11-07 09:31:47 +0700

    update ticketRoute

--- src/routes/ticketRoutes.ts
+++ src/routes/ticketRoutes.ts
@@ -1,9 +1,14 @@
-import { createTicket, getTickets } from "@controllers/ticketController";
+import {
+  createTicket,
+  getTickets,
+  updateTicket,
+} from "@controllers/ticketController";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/", getTickets);
 router.post("/", createTicket);
+router.put("/:id", updateTicket);
 
 export default router;
